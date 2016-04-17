3eed0e14871efc24d57ee75d42439ed702abca7f
asalih <ahmet.salih@windowslive.com>
2016-04-18 00:26:23 +0300

    src

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"html/template"
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
-
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/", IndexHandler)
-	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
-
-	http.ListenAndServe(":9091", r)
-}
-
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Write([]byte("Gorilla!\n"))
-	title := "Hello W!"
-
-	p := &AnyP{Titles: title}
-
-	t, _ := template.ParseFiles("views/index.html")
-	t.Execute(w, p)
-}
-
-type AnyP struct {
-	Titles string
+	InitServer()
 }
